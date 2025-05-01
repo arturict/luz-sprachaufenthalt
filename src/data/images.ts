e155a1e7bef6801582a254ba29575234cd64c456
Artur <artur.fer.cruz@gmail.com>
2025-05-01 20:49:06 +0000

    bilder

--- src/data/images.ts
+++ src/data/images.ts
@@ -1,24 +1,24 @@
 export const images = {
   switzerland: [
     {
-      url: '../content/luzern-1.jpg',
+      url: 'https://github.com/arturict/luz-sprachaufenthalt/blob/main/src/content/luzern-1.png?raw=true',
       alt: 'Kapellbrücke in Luzern',
       caption: 'Notre maison en Suisse / Unser Zuhause in der Schweiz'
     },
     {
-      url: '../content/luzern-2.jpg',
+      url: 'https://github.com/arturict/luz-sprachaufenthalt/blob/main/src/content/luzern-2.jpg?raw=true',
       alt: 'Torbogen in Luzern',
       caption: 'Torbogen Luzern / Arc de Lucerne'   
     }
   ],
   montpellier: [
     {
-      url: '../content/montpellier-1.jpg',
+      url: 'https://github.com/arturict/luz-sprachaufenthalt/blob/main/src/content/montpellier-1.jpg?raw=true',
       alt: 'Coucher de soleil à la Promenade du Peyrou',
       caption: 'Coucher de soleil magique / Magischer Sonnenuntergang'
     },
     {
-      url: '../content/montpellier-2.jpg',
+      url: 'https://github.com/arturict/luz-sprachaufenthalt/blob/main/src/content/montpellier-2.jpg?raw=true',
       alt: 'Statue au coucher du soleil, Promenade du Peyrou',
       caption: 'La statue dorée / Die goldene Statue'
     }
