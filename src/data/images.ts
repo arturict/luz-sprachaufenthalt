8cca7fb12c36892a9b6ffaef427ff4b45ac52b2e
Artur <artur.fer.cruz@gmail.com>
2025-05-01 20:40:53 +0000

    bilder

--- src/data/images.ts
+++ src/data/images.ts
@@ -1,24 +1,24 @@
 export const images = {
   switzerland: [
     {
-      url: '/content/luzern-1.jpg',
+      url: '../content/luzern-1.jpg',
       alt: 'Kapellbrücke in Luzern',
       caption: 'Notre maison en Suisse / Unser Zuhause in der Schweiz'
     },
     {
-      url: '/content/luzern-2.jpg',
+      url: '../content/luzern-2.jpg',
       alt: 'Torbogen in Luzern',
-      caption: 'Torbogen Luzern / Arc de Lucerne'
+      caption: 'Torbogen Luzern / Arc de Lucerne'   
     }
   ],
   montpellier: [
     {
-      url: '/content/montpellier-1.jpg',
+      url: '../content/montpellier-1.jpg',
       alt: 'Coucher de soleil à la Promenade du Peyrou',
       caption: 'Coucher de soleil magique / Magischer Sonnenuntergang'
     },
     {
-      url: '/content/montpellier-2.jpg',
+      url: '../content/montpellier-2.jpg',
       alt: 'Statue au coucher du soleil, Promenade du Peyrou',
       caption: 'La statue dorée / Die goldene Statue'
     }
